8cd899a9aabc8ee43e593af10d5cbb58568e132a
Latif Kaya <latif.kayaa@outlook.com>
2022-04-12 16:59:37 +0600

    Day 38 Car Task done.

--- src/day38_Inheritance/carTask/Car.java
+++ src/day38_Inheritance/carTask/Car.java
@@ -1,4 +1,47 @@
 package day38_Inheritance.carTask;
 
 public class Car {
+
+    public String brand;
+    public String model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand+ ", "+ model+ " has started.");
+    }
+
+    public void drive(){
+        System.out.println(brand+ ", "+ model+ " has driving.");
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+				add a constructor to set all the fields
+			instance methods:
+				start(), drive(),toString()
+ */
