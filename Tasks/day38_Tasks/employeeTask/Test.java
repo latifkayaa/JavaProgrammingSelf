0edd8f5760b29dd27b10d84b4dc02f80f8f162b0
Latif Kaya <latif.kayaa@outlook.com>
2022-04-12 20:15:32 +0600

    tester's class of work method tested in test class

--- Tasks/day38_Tasks/employeeTask/Test.java
+++ Tasks/day38_Tasks/employeeTask/Test.java
@@ -5,5 +5,9 @@ public class Test {
         Developer d1= new Developer("Aaron", 32, 'M', 1, 100000, "Amazon");
         d1.work();
         System.out.println(d1);
+
+        Tester t1= new Tester("Mike", 34, 'M', 10, 90000, "ZeroBank");
+        t1.work();
+        System.out.println(t1);
     }
 }
